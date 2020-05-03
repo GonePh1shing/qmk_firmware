f5a5a9a10d11892a90a906fce50b431624cfd1be
GonePh1shing <goneph1shing@pm.me>
2020-05-03 17:54:08 +0800

    Set rules.mk

--- keyboards/rgbkb/sol/keymaps/GonePh1shing/rules.mk
+++ keyboards/rgbkb/sol/keymaps/GonePh1shing/rules.mk
@@ -6,6 +6,12 @@
 #
 # To keep things clean and tidy, as well as make upgrades easier, only place overrides from the defaults in this file.
 
+# Keycode Options
+MOUSEKEY_ENABLE         = yes        # Mouse keys
+
+# RGB Options
+RGBLIGHT_FULL_POWER     = yes        # Allow maximum RGB brightness for RGBLIGHT or RGB_MATRIX. Otherwise, limited to a safe level for a normal USB-A port
+FULLHAND_ENABLE         = yes        # Enables the additional 24 Full Hand LEDs
 
 # Do not edit past here
 include keyboards/$(KEYBOARD)/post_rules.mk
